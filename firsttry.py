9603a0dad57483f38603d5eb1c6fb20791538e3a
dewerso <147615930+dewerso@users.noreply.github.com>
2023-10-13 17:07:51 +0500

    Update firsttry.py

--- firsttry.py
+++ firsttry.py
@@ -1,5 +1,20 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+   def init(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       super().init()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
 #Игровая сцена:
 back = (200, 255, 255) 
 win_width = 600
